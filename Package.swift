23ebfc276d31e76d5f944e2208daa3ff77ab8cc5
David Pei <david.pei@dramafever.com>
2021-06-24 14:49:15 -0400

    update

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .target(
             name: "Lottie",
-            path: "lottie-swift/src",
+            path: "lottie-ios/Classes/**/*",
             exclude: ["Public/MacOS"]
         )
     ]
